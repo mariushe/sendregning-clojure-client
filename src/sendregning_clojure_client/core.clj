b6f2fd05991ba4d8519284a46a936eff65c68589
marius herrin <mariusherring@gmail.com>
2015-04-19 01:03:35 +0200

    created add lines function

--- src/sendregning_clojure_client/core.clj
+++ src/sendregning_clojure_client/core.clj
@@ -4,3 +4,13 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn prep-new-invoice []
+  {:lines '()})
+
+(defn add-line [inv qty desc]
+  (merge-with conj 
+              inv 
+              {:lines 
+               {:qty qty}}))
+
--- test/sendregning_clojure_client/core_test.clj
+++ test/sendregning_clojure_client/core_test.clj
@@ -2,6 +2,15 @@
   (:require [clojure.test :refer :all]
             [sendregning-clojure-client.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(deftest test-add-line
+  (testing "add one invoice line"
+    (is (= {:lines '({:qty 1})} 
+           (add-line {:lines '()} 1 "desc")))))
+
+(deftest test-add-two-lines
+  (testing "add two invoice lines"
+    (is (= {:lines '({:qty 1} {:qty 2})}
+           (add-line {:lines '({:qty 2})} 1 "desc2")))))
+
+(run-tests)
